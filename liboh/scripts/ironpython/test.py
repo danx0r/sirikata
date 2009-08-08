53bcccb3c136cbc58e49a730c1432c430a4867e1
danx0r <danbmil99@gmail.com>
2009-08-08 11:41:04 -0700

    proxcall and locrequest

--- liboh/scripts/ironpython/test.py
+++ liboh/scripts/ironpython/test.py
@@ -5,39 +5,46 @@ import protocol.MessageHeader_pb2 as pbHead
 from System import Array, Byte
 from Sirikata.Runtime import HostedObject
 
-def fromByteArray(b):
-    return ''.join(chr(c) for c in b)
-
-def toByteArray(p):
-    return Array[Byte](tuple(Byte(ord(c)) for c in p))
-
+import util
 
 class exampleclass:
+    do_once=1
+    objid=0
 
     def reallyProcessRPC(self,serialheader,name,serialarg):
         print "PY:", "Got an RPC named-->" + name + "<--"
         header = pbHead.Header()
-        header.ParseFromString(fromByteArray(serialheader))
+        header.ParseFromString(util.fromByteArray(serialheader))
         if name == "RetObj":
             retobj = pbSiri.RetObj()
             try:
-                retobj.ParseFromString(fromByteArray(serialarg))
+                retobj.ParseFromString(util.fromByteArray(serialarg))
             except:
                 pass
             self.objid = retobj.object_reference
             print "PY: my UUID is:", uuid.UUID(bytes=self.objid)
             self.spaceid = header.source_space
             print "PY: space UUID:", uuid.UUID(bytes=self.spaceid)
-            print "PY: sending LocRequest"
-            body = pbSiri.MessageBody()
-            body.message_names.append("LocRequest")
-            args = pbSiri.LocRequest()
-            args.requested_fields=1
-            body.message_arguments.append(args.SerializeToString())
-            header = pbHead.Header()
-            header.destination_space = self.spaceid
-            header.destination_object = self.objid
-            HostedObject.CallFunction(toByteArray(header.SerializeToString()+body.SerializeToString()), self.callback)
+            self.setPosition(angular_speed=1,axis=(0,1,0))
+
+        elif name == "ProxCall":
+            proxcall = pbSiri.ProxCall()
+            proxcall.ParseFromString(util.fromByteArray(serialarg))
+            objRef = uuid.UUID(bytes=proxcall.proximate_object)
+            print "PY: Proxcall on:", objRef
+            if self.objid and (proxcall.proximate_object!=self.objid):
+                if self.do_once:
+                    self.do_once=0
+                    print "PY: sending LocRequest"
+                    body = pbSiri.MessageBody()
+                    body.message_names.append("LocRequest")
+                    args = pbSiri.LocRequest()
+                    args.requested_fields=1
+                    body.message_arguments.append(args.SerializeToString())
+                    header = pbHead.Header()
+                    header.destination_space = self.spaceid
+                    header.destination_object = proxcall.proximate_object #self.objid
+                    HostedObject.CallFunction(util.toByteArray(header.SerializeToString()+body.SerializeToString()), self.callback)
 
     def processRPC(self,header,name,arg):
         try:
